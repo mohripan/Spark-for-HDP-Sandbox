4ccfcb1da93c2f5f02f1f3835c7da26f3500149d
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2023-03-22 02:11:50 +0700

    Fix bug

--- fakeFriends.py
+++ fakeFriends.py
@@ -1,19 +1,18 @@
-from pyspark import SparkContext, SparkConf
+from pyspark import SparkConf, SparkContext
 
-def parse_line(line):
-    fields = line.split(',')
-    age = int(fields[2])
-    num_friends = int(fields[3])
-    return (age, num_friends)
-
-conf = SparkConf().setMaster('local').setAppName('fakeFriends')
+conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
 sc = SparkContext(conf = conf)
 
-lines = sc.textFile('fakeFriends\fakefriends.csv')
-rdd = lines.map(parse_line)
-totals_by_age = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age, numFriends)
 
-averagesByAge = totals_by_age.mapValues(lambda x: x[0] / x[1])
+lines = sc.textFile("fakeFriends/fakefriends.csv")
+rdd = lines.map(parseLine)
+totalsByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
+averagesByAge = totalsByAge.mapValues(lambda x: x[0] / x[1])
 results = averagesByAge.collect()
 for result in results:
     print(result)
