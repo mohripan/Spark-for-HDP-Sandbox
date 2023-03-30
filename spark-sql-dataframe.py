ab3389b6034ddf87665d739ef0fd86a5b51f2d01
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2023-03-30 15:13:06 +0700

    header

--- spark-sql-dataframe.py
+++ spark-sql-dataframe.py
@@ -2,7 +2,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('SparkSQL').getOrCreate()
 
-people = spark.read.option('header', 'true').option('inferSchema', 'true').csv('fakefriends.csv')
+people = spark.read.option('header', 'true').option('inferSchema', 'true').csv('fakefriends-header.csv')
 
 print('Here is our inferred schema')
 people.printSchema()
